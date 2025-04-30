0aa5ecec06c76f9dc45b392b47927cebda061c9a
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2025-04-30 12:23:37 +0100

    layouts

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,5 +1,16 @@
+import { Header } from "components";
+
 const AllUsers = () => {
-  return <div>Users Table</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Check out our current users in real time."
+      />
+
+      <h1>All Users Page Content</h1>
+    </main>
+  );
 };
 
 export default AllUsers;
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,5 +1,19 @@
+import { Header } from "components";
+
 const Dashboard = () => {
-  return <div>Dashboard page</div>;
+  const user = {
+    name: "John",
+  };
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user.name ?? "Guest"} 👋`}
+        description="Track activity, trends and popular destination in real time."
+      />
+
+      <h1>Dashboard Page content</h1>
+    </main>
+  );
 };
 
 export default Dashboard;
